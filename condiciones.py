b7b0700c39ae0267fcb41686b59beba6069fd64a
vgarcia0906 <97006626+ALEJANDRA-1906@users.noreply.github.com>
2022-01-03 01:02:12 -0500

    Add files via upload

---
+++ condiciones.py
@@ -0,0 +1,44 @@
+#Nombre: Vanessa Alejandra Garcia Cortez
+#curso: C1
+#ASIGNATURA: Técnica de Programación
+#ENUNCIADO:
+#texto= "Star Wars, conocida también en español como La" \
+ #      "guerra de las galaxias, es una franquicia compuesta " \
+       "primordialmente de una serie de películas concebidas por " \
+       "el cineasta estadounidense George Lucas en la década de 1970, " \
+       "y producidas y distribuidas por The Walt Disney Company a partir de " \
+       "2012. Su trama describe las vivencias de un grupo de personajes que " \
+       "habitan en una galaxia ficticia e interactúan con elementos como " \
+       "«la Fuerza», un campo de energía metafísico y omnipresente que " \
+       "posee un «lado luminoso» impulsado por la sabiduría, la nobleza" \
+       "y la justicia y utilizado por los Jedi, y un «lado oscuro» usado " \
+  #     "por los Sith y provocado por la ira, el miedo y el odio".
+
+#numero1=10
+numero2= 20
+numero3= 30
+
+suma = numero1 + \
+       numero2 + \
+       numero3 + \
+
+    #tupla
+t = (4,5
+     6 ,7)
+
+print(t)
+print(texto)
+print(suma)
+
+numero = int(input("Ingrese numero: "))
+if numero >= 0:
+   print("numero es psitivo")
+else:
+   print("nimero negativo")
+
+edad = int(input("ingrese edad: "))
+if edad >= 18
+    print(" Eres mayor de edad!")
+else:
+    print(" Eres menor de edad!")
+else:
